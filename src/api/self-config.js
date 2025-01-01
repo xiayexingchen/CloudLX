d3b3b8200d2b7a4875ed0f620161173444736072
LHappiness <2987366286@qq.com>
2025-01-02 01:38:07 +0800

    ending

--- src/api/self-config.js
+++ src/api/self-config.js
@@ -1,5 +1,6 @@
 const apiConfig = {
   dev: {
+    //后端地址
     // apiUrl: 'http://192.168.31.118:8080',
     apiUrl: 'http://120.46.199.126:8080',
     //apiUrl: 'http://127.0.0.1:4523/m1/5326600-4997107-default' //apifox
